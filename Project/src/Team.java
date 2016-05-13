9fd80c259b1849f49cfa4007b687ec51920a76bc
jonathanlansang <jonathan.lansang1998@gmail.com>
2016-05-13 15:10:21 -0700

    Update Team.java

--- Project/src/Team.java
+++ Project/src/Team.java
@@ -1,6 +1,48 @@
 /**
  * Created by alimousa on 5/4/16.
  */
+/**
+ * Created by jonathanlansang on 5/13/16.
+ */
+import java.util.*;
+
 public class Team {
-    //change 1
+
+    private String name;
+    private Player[] player;
+    private String location;
+    private double wins;
+    private int loss;
+
+    public Team(//ali wants to change format of constructor){
+
+        name = teamName;
+        player = myPlayers;
+        location = teamLocation;
+        wins = myWins;
+        loss = myLosses;
+    }
+    public String getName(){
+        return name;
+    }
+    public getPlayer(String playerName){
+
+    }
+    public String printPlayerName(ArrayList<Player> name){
+        String playerNames= "";
+        for(int i=0; i<name.size(); i++)
+        {
+            playerNames += name.//add the getName method in player class
+        }
+
+    }
+    public String getLocation(){
+        return location;
+    }
+    public String getRecord(){
+        return wins + "-" + loss;
+    }
+    public String toString(){
+        location + " " + name +"/nRecord: " + getRecord() +
+    }
 }
